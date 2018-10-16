83603f4514e7cc7d2a1d363e2713762a1ca66c0e
richardmaccaw <ricci6@gmail.com>
2018-10-16 16:58:38 +0100

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,6 +1,6 @@
 def select_books_titles_and_years_in_first_series_order_by_year
   "
-    SELECT books.title, books.year
+    SELECT title, year
     FROM books
     WHERE books.series_id = 1
     ORDER BY books.year
@@ -18,50 +18,29 @@ end
 
 def select_value_and_count_of_most_prolific_species
   "
-    SELECT species, COUNT(*)
+    SELECT characters.species, COUNT(characters.species)
     FROM characters 
-    GROUP BY species 
-    ORDER BY COUNT(species) DESC 
+    GROUP BY characters.species 
+    ORDER BY COUNT(characters.species) DESC 
     LIMIT 1
-
   "
 end
 
 def select_name_and_series_subgenres_of_authors
   "
-    SELECT authors.name, subgenres.name
-    FROM authors
-    JOIN series
-    ON series.author_id = authors.id
-    JOIN subgenres
-    ON series.subgenre_id = subgenres.id
+    SELECT author.name, subgenres.name
+    FROM 
   "
 end
 
 def select_series_title_with_most_human_characters
   "
-    SELECT series.title 
-    FROM series 
-    JOIN books 
-    ON books.series_id = series.id 
-    JOIN character_books 
-    ON character_books.book_id = books.id 
-    JOIN characters 
-    ON character_books.character_id = characters.id 
-    WHERE characters.species = 'human' 
-    GROUP BY series.title 
-    ORDER BY COUNT(*) DESC 
-    LIMIT 1
+    
   "
+
 end
 
 def select_character_names_and_number_of_books_they_are_in
   "
-    SELECT characters.name, COUNT(*)
-    FROM character_books 
-    JOIN characters 
-    ON character_books.character_id = characters.id 
-    GROUP BY characters.name 
-    ORDER BY COUNT(*) DESC
-  "
+   "
 end
